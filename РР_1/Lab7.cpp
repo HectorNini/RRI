44900ccf07144f5b49e713ba346248ad47dbccf1
HectorNini <117264364+HectorNini@users.noreply.github.com>
2023-04-03 13:22:14 +0300

    Add files via upload

---
+++ РР_1/Lab7.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+double** massiv(int sz)
+{
+    double** A;
+    A = new double* [sz]; 
+    for (int i = 0; i < sz; i++) {
+        A[i] = new double[sz];
+    }
+    return A;
+}
+
+double maximum(double** m,int sz)
+{
+    int z = 0,i=0,j=0;
+    double max_el = -9999999;
+    for (i = (sz - 1); i >= (sz / 2) - 1; i--) {
+        for (j = 0; j < sz; j++) {
+            if ((j >= z) && (j <= (sz - 1 - z))) {
+                if (max_el < m[i][j])
+                    max_el = m[i][j];
+            }
+        }
+        z += 1;
+        cout << endl;
+    }
+    return max_el;
+}
+double minimum(double** m, int sz)
+{
+    int z = 0, i = 0, j = 0;
+    double min_el = 9999999;
+    for (i = (sz - 1); i >= (sz / 2) - 1; i--) {
+        for (j = 0; j < sz; j++) {
+            if ((j >= z) && (j <= (sz - 1 - z))) {
+                if (min_el > m[i][j])
+                    min_el = m[i][j];
+            }
+        }
+        z += 1;
+        cout << endl;
+    }
+    return min_el;
+}
+
+double summa(double** m, int sz)
+{
+    double sum = 0;
+    sum = minimum(m, sz) +maximum(m, sz);
+    return sum;
+}
+
+void fill(double** m, int sz) {
+    int i = 0, j = 0;
+    for (i = 0; i < sz; i++) {
+        for (j = 0; j < sz; j++) {
+            m[i][j] = rand() % 19 - 9;
+            cout << m[i][j] << "   ";
+        }
+        cout << endl;
+    }
+
+}
+
+
+
+
+int main()
+{
+    srand(time(0));
+    setlocale(LC_ALL, "rus");
+    double max = 0,min=0,sum=0;
+    int n = 1;
+    double** mas;
+    cout << "Введите размер двумерного массива N x N" << endl;
+    cout << "N = ";
+    cin >> n;
+    mas = massiv(n);
+    fill(mas,n);
+    //max = maximum(mas, n);
+    //min = minimum(mas, n);
+   // sum = summa(minimum(mas, n), maximum(mas, n));
+    //cout << "Макс = " << max << endl;  
+    //cout << "Мин = " << min << endl;
+    sum = summa(mas, n);
+    cout << "Сумма = " << sum << endl;
+}
+
+
+
---
+++ РР_1/Lab_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double x = 0, y = 0;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    if  ((pow(x, 2) + pow(y, 2) < 1) && ((x < -0.7) || ((x > 0) && abs(y) < 0.3)))
+        cout << "Точка принадлежит области";
+    else if (((x > -0.7) && (x < 0)) || (pow(x, 2) + pow(y, 2) > 1) || ((x > 0) && abs(y) > 0.3))
+        cout << "Точка не принадлежит области";
+    else
+        cout << "Точка лежит на границе области";
+}
+
